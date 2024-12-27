ce0bb15e50da3100d659cd7c0b6a30b0fc483c7f
reemo9 <152188351+amgen1@users.noreply.github.com>
2024-12-27 22:20:38 +0300

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -59,7 +59,7 @@ if ($text === '/start') {
         'reply_markup' => json_encode([
             'inline_keyboard' => [
                 [
-                    ['text' => 'Play XBB Now', 'web_app' => ['url' => 'https://app.companybro.com']],
+                    ['text' => 'Play XBB Now', 'web_app' => ['url' => 'https://xbbcoin-pink.vercel.app/']],
                     ['text' => 'Join Our Community', 'url' => 'https://t.me/xbb_community']]
             ]
         ])
